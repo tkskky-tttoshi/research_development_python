beb869f269357db076b1b6edf3f47c2e92f4ed23
Toshiki Okamura <tkskky.naruto921@gmail.com>
2020-08-07 11:31:26 +0900

    make result_sheet1_arranger.py to arrange it
    delete unnecessary part

--- result_sheet1_arranger.py
+++ result_sheet1_arranger.py
@@ -6,8 +6,6 @@ with open("./result_sheet1.csv") as csv_file:
   reader = csv.reader(csv_file)
   csv_file_array=[row for row in reader]
 
-print(csv_file_array)
-index = csv_file_array[1][0]
 for i in range(len(csv_file_array)):
   index = csv_file_array[i][0]
   csv_file_name = "vehicle_"+index+".csv"
